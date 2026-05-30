97e4ec9019babb9c96b3b678ecebe71ae6ccf088
repa214 <oaoaoammm459@gmail.com>
2026-05-30 17:15:27 +0300

    Version 1.4b

--- libwebp/types.h
+++ libwebp/types.h
@@ -1,14 +1,3 @@
-// Copyright 2010 Google Inc. All Rights Reserved.
-//
-// Use of this source code is governed by a BSD-style license
-// that can be found in the COPYING file in the root of the source
-// tree. An additional intellectual property rights grant can be found
-// in the file PATENTS. All contributing project authors may
-// be found in the AUTHORS file in the root of the source tree.
-// -----------------------------------------------------------------------------
-//
-//  Common types + memory wrappers
-//
 // Author: Skal (pascal.massimino@gmail.com)
 
 #ifndef WEBP_WEBP_TYPES_H_
