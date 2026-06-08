34c26d23b87ae35f8e71fa30fcd677cc631fc8b4
repa214 <oaoaoammm459@gmail.com>
2026-06-08 15:18:33 +0300

    Version 1.4b.2

--- libwebp/types.h
+++ libwebp/types.h
@@ -1,9 +1,5 @@
-// Author: Skal (pascal.massimino@gmail.com)
-
 #ifndef WEBP_WEBP_TYPES_H_
 #define WEBP_WEBP_TYPES_H_
-
-#include <stddef.h>  // IWYU pragma: export for size_t
 
 #ifndef _MSC_VER
 #include <inttypes.h>  // IWYU pragma: export
